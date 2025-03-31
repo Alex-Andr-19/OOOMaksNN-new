a9062374cee9377b84cfb5defb12aa18b53a8cba
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2025-03-31 12:30:30 +0300

    feat: Init

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import { onMounted } from "vue";
+
+onMounted(() => {
+    console.log("Default layout mounted!!!");
+});
+</script>
+
+<template>
+    <div class="dafault-layout">
+        <RouterView />
+    </div>
+</template>
---
+++ src/views/HalloPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+onMounted(() => {
+    console.log("HalloPage mounted!!!");
+});
+</script>
+
+<template>Hallo Page</template>
