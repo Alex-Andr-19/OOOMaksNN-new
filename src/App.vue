dcb5a522c0e53e54bcd5ddefb75a4a804cbd839d
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2025-04-02 14:33:31 +0300

    fix: *

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <script setup lang="ts">
 import DefaultLayout from "@layouts/DefaultLayout.vue";
 import WindowResizeProvider from "./components/WindowResizeProvider.vue";
-import { onMounted } from "vue";
-
-onMounted(() => {
-    console.log("App mounted!!!");
-});
 </script>
 
 <template>
