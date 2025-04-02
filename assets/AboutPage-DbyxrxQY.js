141240a67a3a92d58e0a485b01a0970017d4b7a2
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2025-04-02 14:06:38 +0300

    Updates

--- assets/AboutPage-DbyxrxQY.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-BQ8fLjye.js";const t={};function r(c,o){return"About page"}const n=e(t,[["render",r]]);export{n as default};
--- assets/ProductsPage-CkNaOFGM.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index-BQ8fLjye.js";const c={};function e(t,o){return"Products page"}const _=r(c,[["render",e]]);export{_ as default};
