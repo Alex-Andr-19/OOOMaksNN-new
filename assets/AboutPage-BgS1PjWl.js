b75f204c73f28c04146656a6d4b6ca68df507134
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2025-04-02 14:24:35 +0300

    Updates

--- assets/AboutPage-BgS1PjWl.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-CtXXULmq.js";const t={};function r(c,o){return"About page"}const n=e(t,[["render",r]]);export{n as default};
--- assets/ContactsPage-DbapkgXx.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index-CtXXULmq.js";const c={};function e(n,r){return"Contacts page"}const o=t(c,[["render",e]]);export{o as default};
