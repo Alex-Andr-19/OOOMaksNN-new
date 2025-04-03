a58c1bbaa596356ca4108e081486c441792db476
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2025-04-03 15:49:36 +0300

    fix: Add productNames property for productStore

--- src/stores/productsStore.ts
+++ src/stores/productsStore.ts
@@ -6,10 +6,11 @@ import type { LocaleType } from "@/types/i18n";
 
 export const useProductsStore = defineStore("products", () => {
     const content = ref<Producti18nType>(i18n);
+    const productNames = Object.keys(content.value);
 
     function getContentByName(name: ProductsNames, locale: LocaleType) {
         return content.value[name][locale];
     }
 
-    return { content, getContentByName };
+    return { productNames, getContentByName };
 });
